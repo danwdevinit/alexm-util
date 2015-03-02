731b88e2a172f177d84c8ac8afd69911ad077669
Alex Miller <alex.k.miller@gmail.com>
2015-03-02 13:43:48 +0000

    General interpolation by column name

---
+++ DevInit/R/general_interp.R
@@ -0,0 +1,39 @@
+#install.packages('zoo')
+#install.packages('plyr')
+require(zoo)
+require(plyr)
+
+setwd("C:/git/alexm-util/DevInit/R/tmp")
+
+interpolateCol <- function(data,colname)
+{
+  return(
+    ddply(data,.(id),function(x)
+    {
+      naLen = nrow(x[which(is.na(x[,colname])),])
+      allLen = nrow(x)
+      valueLen = allLen-naLen
+      if(valueLen>=2)
+      {
+        interpVals <- na.approx(x[,colname])
+        xIndex = 1
+        while(is.na(x[,colname][xIndex])){xIndex<-xIndex+1}
+        for(i in 1:length(interpVals))
+        {
+          x[,colname][xIndex] <- interpVals[i]
+          xIndex<-xIndex+1
+        }
+      }
+      return(x)
+    }
+    ))
+}
+
+data <- read.csv("C:/git/digital-platform/country-year/employment-by-sector.csv", header = TRUE,sep=",")
+names <- colnames(data)
+data <- interpolateCol(data,"employment.agriculture")
+data <- interpolateCol(data,"employment.industry")
+data <- interpolateCol(data,"employment.services")
+data <- sapply(data,as.character)
+data[is.na(data)] <- ""
+write.csv(data,"employment-by-sector.csv",row.names=FALSE)
