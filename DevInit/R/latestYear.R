52fe2bdf42e415b4c25c76fac1099c998339c2cc
Alex Miller <alex.k.miller@gmail.com>
2015-03-16 12:12:44 +0000

    LatestYear

---
+++ DevInit/R/latestYear.R
@@ -0,0 +1,51 @@
+#install.packages('plyr')
+require(plyr)
+
+setwd("C:/git/alexm-util/DevInit/R/tmp")
+filenames <- list.files("C:/git/digital-platform/country-year", pattern="*.csv", full.names=TRUE)
+
+latestYear <- function(data)
+{
+  return(
+    ddply(data,.(id),function(x)
+    {
+      naLen <- nrow(x[which(is.na(x$value)),])
+      allLen <- nrow(x)
+      valueLen <- allLen-naLen
+      if(valueLen>=1)
+      {
+        for(i in 1:length(x$value))
+        {
+          if(!is.na(x$value[i])){
+            latestId = x$id[i]
+            latestYear = x$year[i]
+            latestVal = x$value[i]
+          }
+        }
+      }
+      else
+      {
+        latestId = x$id[1]
+        latestYear = max(x$year)
+        latestVal = NA
+      }
+      y <- c(latestYear,latestVal)
+      return(y)
+    }
+    )
+  )
+}
+
+for (i in 1:length(filenames))
+{
+  data <- read.csv(filenames[i], header = TRUE,sep=",",na.strings="",check.names=FALSE)
+  names <- colnames(data)
+  if((length(names)==3 & "id" %in% names & "year" %in% names & "value" %in% names) | (length(names)==4 & "id" %in% names & "year" %in% names  & "value" %in% names & "original-value" %in% names))
+  {
+    data <- data[order(data$id,data$year),]
+    data <- latestYear(data)
+    names(data)[names(data) == "V1"] <- "year"
+    names(data)[names(data) == "V2"] <- "value"
+    write.csv(data,basename(filenames[i]),row.names=FALSE,na="")
+  }
+}
