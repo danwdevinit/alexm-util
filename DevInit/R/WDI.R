56de113bb166ce626d76755523fa0a69355f88fe
Alex Miller <alex.k.miller@gmail.com>
2015-03-30 14:12:08 +0100

    WDI downloader

---
+++ DevInit/R/WDI.R
@@ -0,0 +1,33 @@
+#install.packages('WDI')
+library(WDI)
+
+wd <- "C:/git/digital-platform/country-year/"
+entityPath <- "C:/git/digital-platform/reference/entity.csv"
+entities <- read.csv(entityPath, header = TRUE,na.strings="",check.names=FALSE,stringsAsFactors=FALSE)
+entities <- entities$id
+setwd(wd)
+indicator <- "NY.GNP.MKTP.KD"
+
+dat <- WDI(country = "all", 
+           indicator = indicator, 
+           start = 2000, 
+           end = 2015)
+
+names(dat)[names(dat) == indicator] <- "value"
+names(dat)[names(dat) == "iso2c"] <- "id"
+keep <- c("id","year","value")
+dat <- dat[keep]
+id <- c()
+year <- c()
+value <- c()
+for(i in 1:nrow(dat)){
+  if(dat$id[i] %in% entities){
+    id <- c(id,dat$id[i])
+    year <- c(year,dat$year[i])
+    value <- c(value,dat$value[i])
+  }
+}
+dat <- data.frame(id,year,value)
+dat <- dat[order(dat$id,dat$year),]
+
+write.csv(dat,"gni-usd-2005.csv",row.names=FALSE,na="")
