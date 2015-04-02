71c83866c51acd8f26a5273960c943e851f3bba0
Alex Miller <alex.k.miller@gmail.com>
2015-04-02 13:49:14 +0100

    New results and new Dict

--- DevInit/budgetLevels/fixDict.py
+++ DevInit/budgetLevels/fixDict.py
@@ -7,9 +7,14 @@ with open("./orgDict.json", 'r') as f:
     orgDict = json.load(f)
 
 for country in orgDict:
+    toDelete = []
     for tag in orgDict[country]:
         if "l5" not in orgDict[country][tag]:
             orgDict[country][tag]["l5"] = ""
+        if tag[:2].lower()=="l5":
+            toDelete.append(tag)
+    for tag in toDelete:
+        del orgDict[country][tag]
 with open("./orgDict.json", 'w') as output_file:
     json.dump(orgDict,output_file,ensure_ascii=False,sort_keys=True,indent=2)
 print('Done.')
