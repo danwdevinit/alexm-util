0a3b34649f74e1de5ff44a6d2773e8be4a4a18e4
Alex Miller <alex.k.miller@gmail.com>
2015-04-15 10:55:26 +0100

    Download changes

--- DevInit/CRS/downloadCRS.js
+++ DevInit/CRS/downloadCRS.js
@@ -2,7 +2,6 @@ var request = require('request')
 ,cheerio = require('cheerio')
 ,fs = require('fs')
 ,Download = require('download')
-,glob = require("glob")
 ,url = "https://stats.oecd.org/DownloadFiles.aspx?DatasetCode=CRS1"
 ,dataRoot = "https://stats.oecd.org/FileView2.aspx?IDFile="
 ,filepath=process.argv[2]
@@ -12,25 +11,21 @@ request(url,parseFiles);
 
 function parseFiles(e,r,b){
     if(e){throw e};
-    console.log("List of CRS files successfully fetched.")
+    console.log("List of CRS files successfully fetched. Proceeding to download...")
     var $ = cheerio.load(b,{normalizeWhitespace:true}),
     links = $('a'),
     linkLen = links.length;
     for(var i = 0; i < linkLen; i++){
         var link = links[i],
         onclick = link.attribs.onclick,
-        title = link.children[0].data.split("/")[0].trim().split("-")[0],
+        title = link.children[0].data.split("/")[0].trim(),
         fileID = onclick.substr(15,onclick.length-18).replace(/\_/g,"-");
-        console.log("Checking if "+title+".zip already exists...");
-        if (!fs.existsSync(filepath+title+".zip")) {
-            console.log(title+".zip does not exist. Downloading...");
-            new Download({mode: '755'})
-                .get(dataRoot+fileID)
-                .dest(filepath)
-                .rename(title+".zip")
-                .run(function (err, files) {
-                    console.log(files);
-                });
-        }
+        new Download({mode: '755'})
+            .get(dataRoot+fileID)
+            .dest(filepath)
+            .rename(title+".zip")
+            .run(function (err, files) {
+                console.log(files);
+            });
     };
 };
