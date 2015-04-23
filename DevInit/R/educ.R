1e942cb5aaacfe7df8ca26f333216d7ec4d6e1a5
Alex Miller <alex.k.miller@gmail.com>
2015-04-23 13:17:56 +0100

    Commit

--- DevInit/R/educ.R
+++ DevInit/R/educ.R
@@ -1,11 +1,5 @@
-#List all csvs
-filenames <- list.files("C:/git/alexm-util/DevInit/Uganda/Educ/tmp/", pattern="*.csv", full.names=TRUE)
+wd <- "C:/Users/alexm/Documents/Rwork"
+setwd(wd)
 
-#Iterate through files, reading them in
-message(paste0("Reading ",filenames[1],"..."))
-data <- read.csv(filenames[1], header = TRUE,sep=",",na.strings="",check.names=FALSE)
-for (i in 2:length(filenames))
-{
-  message(paste0("Reading ",filenames[i],"..."))
-  data <- rbind(data,read.csv(filenames[i], header = TRUE,sep=",",na.strings="",check.names=FALSE))
-}
+data <- read.csv("Uganda Primary Leaving Exam Results 2014.csv", header = TRUE,sep=",",na.strings="",check.names=FALSE)
+
