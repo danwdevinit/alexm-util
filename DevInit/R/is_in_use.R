f3e10fa3626a092dba12e9bcef2222e989b2fc60
Alex Miller <alex.k.miller@gmail.com>
2015-11-09 16:20:26 +0000

    code check

---
+++ DevInit/R/is_in_use.R
@@ -0,0 +1,58 @@
+wd <- "C:/git/alexm-util/DevInit/R"
+setwd(wd)
+
+trunc <- function(str){
+  newStr <- substr(str,1,nchar(str)-4)
+  return (newStr)
+}
+
+concepts <- read.csv("C:/git/digital-platform/concepts.csv",na.strings="",as.is=TRUE)
+concepts <- transform(
+  concepts
+  ,hasMapTheme = !is.na(map.theme)
+  )
+keep <- c("id","hasMapTheme")
+concepts <- concepts[keep]
+
+countryYear <- list.files("C:/git/digital-platform/country-year", pattern="*.csv")
+countryYear <- sapply(countryYear,trunc)
+
+refs <- list.files("C:/git/digital-platform/reference", pattern="*.csv")
+refs <- sapply(refs,trunc)
+
+id <- c(countryYear,refs)
+allFiles <- data.frame(id,stringsAsFactors=FALSE)
+
+code <- readLines("C:/git/di-website/wp/wp-content/themes/diConcept/dist/app.js")
+
+used <- logical(nrow(allFiles))
+for(i in 1:nrow(allFiles)){
+  fileName <- allFiles[i,1]
+  inCode <- max(grepl(fileName,code))
+  used[i] <- inCode
+}
+
+allFiles$used <- used
+
+final <- merge(
+  allFiles
+  ,concepts
+  ,by="id"
+  ,all=TRUE
+  )
+
+final[which(is.na(final$used)),]$used <- 0
+final[which(is.na(final$hasMapTheme)),]$hasMapTheme <- FALSE
+
+final <- transform(
+  final
+  ,in_ddh=used | hasMapTheme
+    )
+
+keep <- c("id","in_ddh")
+final <- final[keep]
+
+#We know these are used, even if they're not in the code
+final[which(grepl("oda-donor",final$id)),]$in_ddh <- TRUE
+
+write.csv(final,"in_code_check.csv",na="",row.names=FALSE)
