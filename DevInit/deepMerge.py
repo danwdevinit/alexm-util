e9ac4001a31d2611e12452293b0761a3d5bef353
unknown <alexm@DI0348.DIPR.local>
2015-12-14 12:25:34 -0500

    ordered dict

--- DevInit/deepMerge.py
+++ DevInit/deepMerge.py
@@ -1,8 +1,10 @@
+import collections
+
 def deepMerge(obj1,obj2):
     #iterate through keys
     for key in obj1:
         #if it's a value, we've hit the bottom, copy all of obj2 into obj1
-        if type(obj1[key]) is not dict:
+        if type(obj1[key]) is not collections.OrderedDict:
             for key2 in obj2:
                 obj1[key2] = obj2[key2]
         #if it's a dictionary we need to go deeper
