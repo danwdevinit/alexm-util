d865f96b690ca087ec5bbb0088c341764a010624
Alex Miller <alex.k.miller@gmail.com>
2016-02-02 07:59:06 -0500

    poor people calc

---
+++ DevInit/R/poor-people.R
@@ -0,0 +1,19 @@
+wd<- "C:/git/digital-platform/country-year"
+povPath <- "./poverty-125.csv"
+popPath <- "./population-total.csv"
+setwd(wd)
+
+pov <- read.csv(povPath, header = TRUE,na.strings="",check.names=FALSE,colClasses=c("character","numeric","numeric","numeric"))
+pop <- read.csv(popPath, header = TRUE,na.strings="",check.names=FALSE,colClasses=c("character","numeric","numeric"))
+
+dat <- merge(pov, 
+             pop, 
+             by = c("id","year"),                    
+             sort = FALSE
+             ,suffixes=c(".pov",".pop"))
+dat$value <- dat$value.pov*dat$value.pop
+keep <- c("id","year","value")
+dat <- dat[keep]
+dat <- dat[order(dat$id,dat$year),]
+
+write.csv(dat,"poor-people.csv",row.names=FALSE,na="")
