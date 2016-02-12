ca6b8563d90e929e5e154682067f82f47a5273d0
Alex Miller <alex.k.miller@gmail.com>
2016-02-12 09:04:41 -0500

    sample code

--- DevInit/R/GHA/fts_from_file.R
+++ DevInit/R/GHA/fts_from_file.R
@@ -8,3 +8,27 @@ data <- transform(data,Original.currency.amount=as.numeric(gsub(",","", Original
 data <- transform(data,USD.pledged=as.numeric(gsub(",","", USD.pledged)))
 data <- transform(data,Project.current.request=as.numeric(gsub(",","", Project.current.request)))
 data <- transform(data,Item.ID=as.numeric(gsub(",","", Item.ID)))
+
+#Sample transform to new column
+data <- transform(data,millionsContributed=USD.committed.contributed/1000000)
+
+#construct a fake deflator with donor, year, and a random value
+donorList <- unique(data$Donor)
+fakeDeflator <- runif(length(donorList))
+fakeDeflatorYear <- rep(2016)
+deflator <- data.frame(donorList,fakeDeflatorYear,fakeDeflator)
+#Make deflator names match data
+names(deflator) <- c("Donor","Appeal.year","deflator")
+#Add deflator to dataset by matching unique combinations of donor and appeal year
+data <- merge(
+  data
+  ,deflator
+  ,by=c("Donor","Appeal.year")
+  ,all.x=TRUE
+  )
+#Create deflated value by multiplying contributed*deflator
+data <- transform(data,contributedDeflated=USD.committed.contributed*deflator)
+
+#Example String transformations
+data <- transform(data,USAisDONOR = (Donor=="United States of America") )
+
