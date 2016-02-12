ba8bf4228dce705dfd8fcff6a6ca3851fd0dec09
Alex Miller <alex.k.miller@gmail.com>
2016-02-12 08:53:56 -0500

    sample code

--- DevInit/R/GHA/fts_from_file.R
+++ DevInit/R/GHA/fts_from_file.R
@@ -1,3 +1,10 @@
 wd <- "D:/Documents/"
 setwd(wd)
 data <- read.csv("fts.csv",header=TRUE,skip=5,na.strings="",as.is=TRUE)
+
+#Parse numbers (removing commas)
+data <- transform(data,USD.committed.contributed=as.numeric(gsub(",","", USD.committed.contributed)))
+data <- transform(data,Original.currency.amount=as.numeric(gsub(",","", Original.currency.amount)))
+data <- transform(data,USD.pledged=as.numeric(gsub(",","", USD.pledged)))
+data <- transform(data,Project.current.request=as.numeric(gsub(",","", Project.current.request)))
+data <- transform(data,Item.ID=as.numeric(gsub(",","", Item.ID)))
