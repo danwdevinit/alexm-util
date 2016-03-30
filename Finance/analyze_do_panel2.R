2ee4084e54a1d02fa4df971db5a25b0e86a08f85
Alex Miller <alex.k.miller@gmail.com>
2016-03-30 14:12:02 -0400

    Write stream

---
+++ Finance/analyze_do_panel2.R
@@ -0,0 +1,118 @@
+####Command####
+#install.packages("ggplot2")
+#install.packages("dplyr")
+library(ggplot2)
+library(plyr)
+library(scales)
+
+input <- "spx"
+
+windows <- TRUE
+if(windows){pathpre<-"C:"}else{pathpre<-"~"}
+wd <- paste0(pathpre,"/git/alexm-util/Finance/")
+setwd(wd)
+now <- Sys.Date()
+dateFolder <- paste0("data/",input,"_",now,"/")
+dir.create(dateFolder)
+
+infilename <- paste0("data/",input,".csv")
+raw <- read.csv(
+  infilename
+  ,header=FALSE
+  ,skip=7
+  ,as.is=TRUE
+)
+splitdate <- function(x){
+  return(gsub(" ","_",strsplit(x," (",fixed=TRUE)[[1]][1]))
+}
+dates <- unique(raw[,1])
+dates <- subset(dates,dates!="")
+dates <- sapply(dates,splitdate)
+
+setClass("myDate")
+setAs("character","myDate", function(from) as.Date(from, format="%m/%d/%y"))
+
+if(exists("command")){rm(command)}
+for(i in 1:length(dates)){
+  outfilename <- paste0(dateFolder,dates[i],input,".csv")
+  if(!exists("command")){
+    command <- paste("node ha_tos2.js",infilename,(i-1),outfilename)
+  }else{
+    command <- paste(command,"& node ha_tos2.js",infilename,(i-1),outfilename)
+  }
+}
+print(command)
+####Create visuals####
+
+if(exists("panelAverages")){rm(panelAverages)}
+for(i in 1:length(dates)){
+  outfilename <- paste0(dateFolder,dates[i],input,".csv")
+  message(outfilename)
+  df <- read.csv(outfilename
+                 ,header=TRUE
+                 ,colClasses=c("myDate","numeric","character","numeric","numeric","numeric"))
+  if(!exists("panelAverages")){
+    panelAverages <- ddply(df,.(date,state,strike),summarize,avg=mean(prob),sd=sd(prob),count=length(prob))
+    
+  }else{
+    panelAverage <- ddply(df,.(date,state,strike),summarize,avg=mean(prob),sd=sd(prob),count=length(prob))
+    panelAverages <- rbind(panelAverages,panelAverage)
+  }
+  timestamp <- paste0(
+    "\nExpiry: "
+    ,df[1,1]
+    ,"\nCalculated: "
+    ,Sys.Date()
+  )
+  
+  change_averages <- ddply(df,.(change,state),summarize,avg=mean(prob))
+  title <- paste0(
+    "Probability of up/neutral/down given percent change",
+    timestamp
+  )
+  p1 <- ggplot(data=change_averages,aes(x=change,y=avg,group=state,colour=state)) +
+    geom_line() +
+    geom_point() +
+    ggtitle(title)
+  ggsave(paste0(dateFolder,"p1-",df[1,1],".jpg"),plot=p1,height=7,width=14)
+  
+  strike_averages <- ddply(df,.(strike,state),summarize,avg=mean(prob),sd=sd(prob),count=length(prob))
+  title <- paste0(
+    "Probability of up/neutral/down given strike",
+    timestamp
+  )
+  p2 <- ggplot(data=strike_averages,aes(x=strike,y=avg,ymax=avg+sd,ymin=avg-sd,group=state,colour=state)) +
+    geom_point(size=3) +
+    geom_errorbar(width=0.2) +
+    ggtitle(title)
+  ggsave(paste0(dateFolder,"p2-",df[1,1],".jpg"),plot=p2,height=7,width=14)
+  
+  #Density
+  jpeg(
+    file=paste0(dateFolder,"p3-",df[1,1],".jpg")
+    ,width=14
+    ,height=7
+    ,res=300
+    ,units="in"
+  )
+  par(mfrow=c(3, 1))
+  states <- unique(df$state)
+  for (i in 1:length(states)) {
+    d <- density(df[which(df$state==states[i]),]$prob)
+    plot(d, type="n", main=paste("Kernal density",states[i],"states",timestamp))
+    polygon(d,  border="#ba0c2f", col="#ba0c2f")
+  }
+  dev.off()
+  par(mfrow=c(1, 1))
+}
+underlying <- 233.72
+percent <- 0.05
+# sub <- subset(averages,strike>=(underlying*(1-percent)) & strike<=(underlying*(1+percent)))
+sub <- panelAverages
+limited_averages <- ddply(sub,.(date,state),summarize,avg=mean(avg),sd=mean(sd),count=sum(count))
+p3 <- ggplot(data=limited_averages,aes(x=date,y=avg,ymax=avg+sd,ymin=avg-sd,group=state,colour=state)) +
+  scale_x_date(date_breaks = "2 months") +
+  geom_line(size=1) +
+  geom_point(size=3) +
+  geom_errorbar(width=0.2)
+ggsave(paste0(dateFolder,"p4.jpg"),plot=p3,height=7,width=14)
---
+++ Finance/ha_tos2.js
@@ -0,0 +1,143 @@
+#!/usr/bin/env node --stack-size=65500
+var basicCSV = require('basic-csv'),
+syl = require('sylvester'),
+cmb = require('cmb'),
+moment = require('moment'),
+fs = require('fs'),
+csvFile = process.argv[2],
+underlying = 0,
+upState = 0,
+downState = 0,
+expiriesAhead = process.argv[3]?parseInt(process.argv[3]):0,
+date="",
+data = {},
+wstream = fs.createWriteStream(process.argv[4]),
+changes = [];
+for(var i = 1; i<41; i++){
+    changes.push(i/2);
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+function CBOEdate(string){
+    var monthStr = string.substr(0,3),
+    weekStr = string.split(" ")[0][3]?parseInt(string.split(" ")[0][3]):3,
+    yearStr = string.split(" ")[1],
+    fridays = ["",moment(monthStr+" "+yearStr,"MMM YY").day("Friday"),moment(monthStr+" "+yearStr,"MMM YY").day("Friday").add(1,'w'),moment(monthStr+" "+yearStr,"MMM YY").day("Friday").add(2,'w'),moment(monthStr+" "+yearStr,"MMM YY").day("Friday").add(3,'w'),moment(monthStr+" "+yearStr,"MMM YY").day("Friday").add(4,'w'),moment(monthStr+" "+yearStr,"MMM YY").day("Friday").add(5,'w')];
+    date = fridays[weekStr];
+    return date;
+};
+
+function isCBOEdate(string){
+    var monthStr = string.substr(0,3),
+    weekStr = string.split(" ")[0][3]?parseInt(string.split(" ")[0][3]):3,
+    yearStr = string.split(" ")[1]?string.split(" ")[1]:false,
+    date = moment(monthStr+" "+yearStr,"MMM YY");
+    return date.isValid();
+};
+
+basicCSV.readCSV(csvFile, {dropHeader: true}, function (error, rows) {
+    underlying = rows[2][0];
+    var rawData = rows.slice(6,rows.length);
+    var dateIndicies = [];
+    for(var i = 0; i < rawData.length; i++){
+        var row = rawData[i];
+        if(isCBOEdate(row[0])){
+            var date = CBOEdate(row[0]);
+            dateObj = {"date":date.format("MM/DD/YY"),"index":i};
+            dateIndicies.push(dateObj);
+        };
+    };
+    for(var i = 0; i < dateIndicies.length; i++){
+        var thisIndex = dateIndicies[i],
+        nextIndex = dateIndicies[i+1]?dateIndicies[i+1]:rawData.length,
+        rawSlice = rawData.slice(thisIndex.index,nextIndex.index);
+        data[thisIndex.date] = [];
+        for(var j = 0; j < rawSlice.length; j++){
+            var row = rawSlice[j];
+            if(isNumber(row[10]) && row[10]>0){
+                //Calls
+                if(isNumber(row[2]) && row[2]>0){
+                    var optObj = {};
+                    optObj.call = 1;
+                    optObj.strike = parseFloat(row[10]);
+                    optObj.price = parseFloat(row[2]);
+                    data[thisIndex.date].push(optObj);
+                };
+                //Puts
+                if(isNumber(row[15]) && row[15]>0){
+                    var optObj = {};
+                    optObj.call = 0;
+                    optObj.strike = parseFloat(row[10]);
+                    optObj.price = parseFloat(row[15]);
+                    data[thisIndex.date].push(optObj);
+                };
+            };
+        };
+    };
+    analyze();
+});
+
+function analyze(){
+    var dates = Object.keys(data);
+    date = dates[expiriesAhead];
+    wstream.write("date,change,state,prob,call,strike\n");
+    for(var j = 0; j<changes.length; j++){
+        var change = changes[j],
+        upState = underlying*(1+(change/100)),
+        downState = underlying*(1-(change/100));
+        options = data[date],
+        Farr = [],
+        Parr = [];
+        for(var i = 0; i < options.length; i++){
+            var option = options[i],
+            strike = option.strike,
+            price = option.price,
+            call = option.call;
+            Parr.push(price);
+            if(call){
+                var upVal = Math.max(upState-strike,0),
+                neuVal = Math.max(underlying-strike,0),
+                downVal = Math.max(downState-strike,0);
+            }else{
+                var upVal = Math.max(strike-upState,0),
+                neuVal = Math.max(strike-underlying,0),
+                downVal = Math.max(strike-downState,0);
+            };
+            var rowData = {"row":[downVal,neuVal,upVal],"call":call,"strike":strike};
+            Farr.push(rowData);
+        };
+        var range = [];
+        for(var i = 0; i < Farr.length; i++){range.push(i);};
+        var com = new cmb(range,3);
+        com.each(
+            function(val){
+                var F = $M([Farr[val[0]].row,Farr[val[1]].row,Farr[val[2]].row]),
+                P = $M([Parr[val[0]],Parr[val[1]],Parr[val[2]]]);
+                if(F.inv()){
+                    var arrowPrices = F.inv().x(P).toArray(),
+                    arrowFlat = [];
+                    arrowFlat = arrowFlat.concat.apply(arrowFlat,arrowPrices);
+                    var arrowMax = Math.max.apply(Math, arrowFlat),
+                    arrowMin = Math.min.apply(Math, arrowFlat),
+                    arrowSum = arrowFlat.reduce(function(a,b){return a+b;});
+                    if(arrowMin>=0 && arrowMax>0 && arrowMax<=1 && arrowSum<=1){
+                        var inducedProb = arrowFlat.map(function(num){return num/arrowSum;});
+                        wstream.write(date+","+change+",down,"+inducedProb[0]+","+Farr[val[0]].call+","+Farr[val[0]].strike+"\n");
+                        wstream.write(date+","+change+",down,"+inducedProb[0]+","+Farr[val[1]].call+","+Farr[val[1]].strike+"\n");
+                        wstream.write(date+","+change+",down,"+inducedProb[0]+","+Farr[val[2]].call+","+Farr[val[2]].strike+"\n");
+                        wstream.write(date+","+change+",neutral,"+inducedProb[1]+","+Farr[val[0]].call+","+Farr[val[0]].strike+"\n");
+                        wstream.write(date+","+change+",neutral,"+inducedProb[1]+","+Farr[val[1]].call+","+Farr[val[1]].strike+"\n");
+                        wstream.write(date+","+change+",neutral,"+inducedProb[1]+","+Farr[val[2]].call+","+Farr[val[2]].strike+"\n");
+                        wstream.write(date+","+change+",up,"+inducedProb[2]+","+Farr[val[0]].call+","+Farr[val[0]].strike+"\n");
+                        wstream.write(date+","+change+",up,"+inducedProb[2]+","+Farr[val[1]].call+","+Farr[val[1]].strike+"\n");
+                        wstream.write(date+","+change+",up,"+inducedProb[2]+","+Farr[val[2]].call+","+Farr[val[2]].strike+"\n");
+                    };
+                };
+            }
+        );
+    };
+    wstream.end();
+};
